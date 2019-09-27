65285d1d26bc206ad67f5b645caf5be73f3804c0
Daniel Cefram Ramirez <me@danielcefram.com>
2019-09-27 18:59:46 +0800

    feat: finalize test app

--- README.md
+++ README.md
@@ -6,9 +6,19 @@ test it out manually.
 Pre-req: npm link the local xjs directory.
 
 ```
+# first, install all of the dependencies (ie. svelte stuff)
+npm i
+
 # in xjs 3.0 directory
 npm link
 
 # in xjs-playground directory
 npm link xjs
+
+# now we can run the test app!
+npm run dev
 ```
+
+Some coolkid gif
+
+![Item Shuffler](xjs-test-app-1.gif "Simple item shuffling")
--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,57 @@
 <script>
-	export let name;
+  import Xjs from "xjs";
+  import ItemProps from "xjs/dist/props/item-props";
+  export let name;
+
+  const xjs = new Xjs();
+
+  function asyncReduce(stack, fn, def) {
+    return stack.reduce(
+      (promise, item, index) =>
+        promise.then(previous => fn(previous, item, index)),
+      Promise.resolve(def)
+    );
+  }
+
+  async function handleClick() {
+    const currentScene = await xjs.getView(0).getCurrentScene();
+    const items = await currentScene.getItems();
+
+    console.log(items);
+
+    // Get their positions
+    const positions = await asyncReduce(
+      items,
+      async (stack, item) => {
+        const pos = await item.getProperty(ItemProps.position);
+
+        return [...stack, pos];
+      },
+      []
+    );
+
+    // Now assign their positions randomly
+    for (let cnt = 0; cnt < items.length; cnt++) {
+      const idx = Math.floor(Math.random() * positions.length);
+      const position = positions.splice(idx, 1);
+      items[cnt].setProperty(ItemProps.position, position[0]);
+    }
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  :global(body) {
+    background-color: #eee;
+  }
+
+  h4 {
+    color: #555;
+    font-weight: normal;
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>XJS 3.0 Test App</h1>
+
+<h4>This test app can shuffle items in the current scene!</h4>
+
+<button on:click={handleClick}>Shuffle items!</button>
